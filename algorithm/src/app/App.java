543e6d8b64669bace9615da92838acae419bde4d
Richard <liujh88888@sina.cn>
2019-06-19 15:34:10 +0000

    include stdlib

---
+++ algorithm/src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+import edu.princeton.cs.algs4.StdOut;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = "java";
+        StdOut.printf("hello %s",name);
+    }
+}
